b31ed13bab1cf2643ea263e78210de2e5736ef2b
Kushal Nandwani <kushalnandwani98@gmail.com>
2023-03-22 18:51:06 +0530

    added react: colors ui

---
+++ block-BRaaay/colors-ui/src/component/Sidebar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <aside className="container">
+      <h2>Colors</h2>
+      <ul>
+        <li>{}</li>
+      </ul>
+    </aside>
+  );
+};
+
+export default Sidebar;
